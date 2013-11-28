63937aeaa47b570caa1b40cb2d2cddb723fef453
Niko Dittmann <mail+git@niko-dittmann.com>
2013-11-28 16:54:08 +0100

    + the actual files

---
+++ anti_backspace.js
@@ -0,0 +1,14 @@
+document.onkeydown = function(e) {stopDefaultBackspaceBehaviour(e);}
+document.onkeypress = function(e) {stopDefaultBackspaceBehaviour(e);}
+
+function stopDefaultBackspaceBehaviour(event) {
+	var event = event || window.event;
+	if (event.keyCode == 8) {
+		var elements = "HTML, BODY, TABLE, TBODY, TR, TD, DIV";
+		var d = event.srcElement || event.target;
+		var regex = new RegExp(d.tagName.toUpperCase());
+		if (regex.test(elements)) {
+			event.preventDefault ? event.preventDefault() : event.returnValue = false;
+		}
+	  }
+}
